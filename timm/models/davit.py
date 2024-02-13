22893f28f5ed3c56f146c8b93717e3a60fee1758
Sanskar021995 <sanskar.gupta@originhealth.ai>
2024-02-13 09:58:46 +0000

    changed the key name as per the key name returned from state dict

--- timm/models/davit.py
+++ timm/models/davit.py
@@ -23,7 +23,7 @@ def _cfg(url='', **kwargs):
         'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': None,
         'crop_pct': .9, 'interpolation': 'bicubic', 'fixed_input_size': True,
         'mean': IMAGENET_DEFAULT_MEAN, 'std': IMAGENET_DEFAULT_STD,
-        'first_conv': 'patch_embeds[0].proj', 'classifier': 'head',
+        'first_conv': 'patch_embeds.0.proj', 'classifier': 'head',
         **kwargs
     }
 
